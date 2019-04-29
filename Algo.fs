556b6056ce0bcea3c080ddf578f45f67a78de123
Hiroaki Wada <pp.790strasse@gmail.com>
2019-04-30 02:45:33 +0900

    コンパレータ実装

--- Algo.fs
+++ Algo.fs
@@ -1,12 +1,15 @@
 ﻿(*
-Algo.fs
-
-Copyright (c) 2019 Hiroaki Wada
-
-This software is released under the MIT License.
-http://opensource.org/licenses/mit-license.php
+  Algo.fs
+  Copyright (c) 2019 Hiroaki Wada
+  This software is released under the MIT License.
+  http://opensource.org/licenses/mit-license.php
 *)
 open System
+
+type Math private() =
+    static let rand = Random();
+    static member Next(arg : int) : int =
+        rand.Next(arg)
 
 type Color =
     | Black = 0
@@ -35,6 +38,23 @@ let showCards cards =
         Console.ResetColor()
         Console.Write(" ")
         )
+    Console.WriteLine("")
+
+let algoComparator cards: List<AlgoCard> = 
+    cards |> List.sortWith(fun x y -> 
+        match x.Number < y.Number, x.Number = y.Number, x.Color = Color.Black with
+        | true, true, true -> -1
+        | true, true, false -> -1
+        | true, false, false -> -1
+        | true, false, true -> -1
+        | false, true, true -> -1
+        | false, true, false -> 1
+        | false, false, true -> 1
+        | false, false, false -> 1
+    )
+
+let shuffle cards = 
+    cards |> List.sortBy(fun _ -> Guid.NewGuid())
 
 [<EntryPoint>]
 let main argv = 
@@ -43,12 +63,15 @@ let main argv =
         cards <- List.append [AlgoCard(i, Color.Black)] cards
         cards <- List.append [AlgoCard(i, Color.White)] cards
 
-    cards |> List.sortBy(fun x -> x.Number + int x.Color) |> showCards
+    let shuffledCards = cards |> shuffle
+    shuffledCards |> showCards
 
-    // cards |> showCards
+    // スライス的な何か
+    //cards.[5*i..5*(i+1)-1]
     
-    // sort用に同数なら白を強い扱いにするコンピレーターを作る
-
+    // sort用に同数なら白を強い扱いにするコンパレーターを作る
+    cards |> algoComparator |> showCards
+    
     // 残からランダムドローして、アタック or 下がるか決める
     // アタックの場合は、何枚目を指定するか決める。（番号だと誤認するので、アルファベット降った方がよい？）
     // 成功したらオープンにする。そしてもっかいアタックするか決める。
